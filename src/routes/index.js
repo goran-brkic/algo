ab98d254180b522633d9e13775df8efe2766b7f9
Goran Brkić <gb51908@fer.hr>
2023-01-17 20:25:27 +0100

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const Binance = require('node-binance-api');
+const binance = new Binance().options({
+  APIKEY: '1IZJSGFkFhSoChofgEjByvFNSYD45t0gNRTmcd8d9AGlSxDNKDYcUVCxvVSHdYf6',
+  APISECRET: 'b9ZP9vXrrXvGaGEzCbFAUiaL3wYuUqXWJRlcjzeilv9KY4Vb14598qnOIvpJMeAc'
+});
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   res.render('index', { title: 'HTML Igra'});
+});
+
+router.get('/webhook', async function(req, res, next) {
+    console.info( await binance.futuresBalance() );
+
+    res.redirect('/')
 });
 
 
