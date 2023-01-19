4f86133f38620eb25924a2f6d4fd2944481f38d7
Goran Brkić <gb51908@fer.hr>
2023-01-19 22:47:06 +0100

    algo 2.0

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ router.get('/', async function(req, res, next) {
   res.render('index', { title: 'HTML Igra'});
 });
 
-router.get('/webhook', async function(req, res, next) {
+router.post('/webhook', async function(req, res, next) {
     /* console.info( await binance.futuresBalance() );
     console.info( await binance.futuresMarkPrice( "BTCUSDT" ) );
     */
@@ -20,6 +20,9 @@ router.get('/webhook', async function(req, res, next) {
     console.info( await binance.futuresCancelAll( "BTCUSDT" ) );
     let result = await binance.futuresMarkPrice( "BTCUSDT" );
     let quantity = (1000 / result.markPrice).toFixed(3)
+    /* let balances = await binance.futuresBalance()
+    if(balances[6].balance < 10)
+      quantitiy = () */
     
     //console.log(quantity)
     console.info( await binance.futuresMarketBuy( 'BTCUSDT', quantity ) );
@@ -33,16 +36,15 @@ router.get('/webhook', async function(req, res, next) {
 
     console.log(entryPrice)
     
-    let price = (entryPrice*0.994).toFixed(1);
+    let price = (entryPrice*0.995).toFixed(1);
     console.log(price)
     let order = await binance.futuresMarketSell("BTCUSDT", quantity, {type: "STOP_MARKET", stopPrice: price, reduceOnly: true});
     console.log(order)
     
-    price = (entryPrice*1.004).toFixed(1);
+    price = (entryPrice*1.002).toFixed(1);
     order = await binance.futuresMarketSell("BTCUSDT", quantity, {type: "TAKE_PROFIT_MARKET", stopPrice: price, reduceOnly: true});
     console.log(order)
 
-    res.render('index', { title: 'HTML Igra'});
 });
 
 
