968fe36398a170e4f72c51e975959955e9cd4dcf
Goran Brkić <gb51908@fer.hr>
2023-01-23 00:22:30 +0100

    algo 3.0

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const Binance = require('node-binance-api');
-const binance = new Binance().options({
-  APIKEY: '1IZJSGFkFhSoChofgEjByvFNSYD45t0gNRTmcd8d9AGlSxDNKDYcUVCxvVSHdYf6',
-  APISECRET: 'b9ZP9vXrrXvGaGEzCbFAUiaL3wYuUqXWJRlcjzeilv9KY4Vb14598qnOIvpJMeAc'
-});
+const Binance = require('binance-api-node').default;
+
+// Authenticated client, can make signed calls
+const client = Binance({
+  apiKey: '1IZJSGFkFhSoChofgEjByvFNSYD45t0gNRTmcd8d9AGlSxDNKDYcUVCxvVSHdYf6',
+  apiSecret: 'b9ZP9vXrrXvGaGEzCbFAUiaL3wYuUqXWJRlcjzeilv9KY4Vb14598qnOIvpJMeAc'
+})
+
+client.time().then(time => console.log(time))
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
@@ -13,66 +17,36 @@ router.get('/', async function(req, res, next) {
 
 router.post('/webhook', async function(req, res, next) {
 
-    console.info( await binance.futuresCancelAll( "BTCUSDT" ) );
+  //console.info( await binance.cancelAll("BTCUSDT") );
 
-    if(req.body.content == 'L'){
-      let result = await binance.futuresMarkPrice( "BTCUSDT" );
-      let quantity = (1000 / result.markPrice).toFixed(3)
-      let balances = await binance.futuresBalance()
-      if(balances[6].balance < 10)
-        quantity = ((balances[6].balance)*100 / result.markPrice).toFixed(3)
+  let price;
+  let response = await client.marginIsolatedAccount({ symbols: 'BTCUSDT'});
+  price = response.assets[0].indexPrice;
 
-      let acc = await binance.futuresAccount();
-      let entryPrice = 0.0;
-      acc.positions.forEach(element => {
-        if(element.symbol == "BTCUSDT")
-          entryPrice = element.entryPrice;
-      });
-      
-      let price = (entryPrice*0.995).toFixed(1);
-      //console.log(price)
-      let order = await binance.futuresMarketSell("BTCUSDT", quantity, {type: "STOP_MARKET", stopPrice: price, reduceOnly: true});
-      //console.log(order)
-      
-      price = (entryPrice*1.002).toFixed(1);
-      order = await binance.futuresMarketSell("BTCUSDT", quantity, {type: "TAKE_PROFIT_MARKET", stopPrice: price, reduceOnly: true});
-      //console.log(order)
-    } else if(req.body.content == 'CLES') {
-        let acc = await binance.futuresAccount();
-        let quantity = 0.0;
-        acc.positions.forEach(element => {
-          if(element.symbol == "BTCUSDT")
-            quantity = element.positionAmt
-        });
-        quantity = quantity.toFixed(3)
-        let order = await binance.futuresMarketSell("BTCUSDT", quantity, {reduceOnly: true})
+  let quantityyy = 100 / price;
+  
+  if(req.body.content == 'buy'){
 
-        let balances = await binance.futuresBalance()
-        if(balances[6].balance < 10)
-          quantity = ((balances[6].balance)*100 / result.markPrice).toFixed(3)
-        else
-          quantity = (1000 / result.markPrice).toFixed(3)
-        
-        order = await binance.futuresMarketSell("BTCUSDT", quantity);
-    } else if(req.body.content == "CL") {
-        let acc = await binance.futuresAccount();
-        let quantity = 0.0;
-        acc.positions.forEach(element => {
-          if(element.symbol == "BTCUSDT")
-            quantity = element.positionAmt
-        });
-        quantity = quantity.toFixed(3)
-        let order = await binance.futuresMarketSell("BTCUSDT", quantity, {reduceOnly: true})
-    } else if(req.body.content == "CS") {
-        let acc = await binance.futuresAccount();
-        let quantity = 0.0;
-        acc.positions.forEach(element => {
-          if(element.symbol == "BTCUSDT")
-            quantity = element.positionAmt
-        });
-        quantity = quantity.toFixed(3)
-        let order = await binance.futuresMarketBuy("BTCUSDT", quantity, {reduceOnly: true} )
-    }
+    quantityyy = 100 / price;
+    console.log(await client.marginOrder({ 
+      symbol: 'BTCUSDT', 
+      isIsolated: 'TRUE',
+      type: 'MARKET',
+      side: 'BUY',
+      quantity: quantityyy
+    }));
+
+  } else if(req.body.content == 'sell'){
+    
+    console.log(await client.marginOrder({ 
+      symbol: 'BTCUSDT', 
+      isIsolated: 'TRUE',
+      type: 'MARKET',
+      side: 'SELL',
+      quantity: quantityyy
+    }));
+
+  }
 
 });
 
